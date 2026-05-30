f19cbd9f044b40ec0bf25e66c9d29def9edec5d8
alainflores-T <alain.flores@ucb.edu.bo>
2026-05-30 13:03:34 -0400

    Delete tiendaweb-backend directory

--- tiendaweb-backend/Datos/Materia.cs
+++
@@ -1,7 +0,0 @@
-namespace tiendaweb_backend.Datos;
-
-public class Materia
-{
-    public string? NombreMateria {get; set; }
-    public int ? PesoMateria { get; set; }
-}
--- tiendaweb-backend/Datos/Usuario.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace tiendaweb_backend.Datos;
-
-public class Usuario
-{
-    public int Id { get; set; }
-    public string nombre { get; set; }
-    public string email { get; set; }
-    public string contrasena { get; set; }
-}
